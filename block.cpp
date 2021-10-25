708955b4f2c933df48b8c92e4707a9fe4db2b571
Giovanni Gasparini <giovannigasparini@ucsb.edu>
2021-10-24 17:08:09 -0700

    add block.cpp to repo

---
+++ block.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num;
+  int rows;
+  int cols;
+  cout << "Enter number of rows and columns:"<<endl;
+  while((cin>>num)&&(num !=0)){
+      rows = num;
+      cin>>cols;
+    for (int i = 0;i<rows;i++){
+        for (int i = 0; i<cols; i++){
+          cout<< "X.";
+        }
+      cout<<endl;
+    }
+    cout << endl<<"Enter number of rows and columns:"<<endl;
+  }
+  return 0;
+}
