18a5bab44b0b94a2ff844e4483bc3e873d824385
Giovanni Gasparini <giovannigasparini@ucsb.edu>
2021-10-24 18:04:19 -0700

    add pi.cpp to repo

---
+++ pi.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double pi=0.0;
+	int n;
+	cout<<"Enter the value of the parameter 'n' in the Leibniz formula ";
+	cout<<"(or -1 to quit):"<<endl;
+	while ((cin >>n)&&(n>=0)){
+		for (int i = 0;i<n;i++){
+			pi += ((pow(-1,i))/((2*i)+1));
+		} 
+		cout.setf(ios::fixed);
+		cout.setf(ios::showpoint);
+		cout.precision(3);
+		cout<<"The approximate value of pi using "<<n<<"  term is: "<<pi*4.0<<endl;
+		pi = 0.0;
+		cout<<"Enter the value of the parameter 'n' in the Leibniz formula ";
+                cout<<"(or -1 to quit):"<<endl;
+	}
+
+
+	return 0;
+}
